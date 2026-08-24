e548cfe895c114d7bb6097126b873a7909e5b360
ahusan1 <122188486+ahusan1@users.noreply.github.com>
2026-08-24 22:32:54 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gbvglgjxfahbjlxvyfvt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdidmdsZ2p4ZmFoYmpseHZ5ZnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MDQ3MTAsImV4cCI6MjA5MzQ4MDcxMH0.P6fYvdlaF3ing_mnu8BgR3odi4cC-WjQ-L0lOhR-7fE';
+const supabaseUrl = 'https://robvniipjwxmxkudghyv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvYnZuaWlwand4bXhrdWRnaHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1ODg1NDUsImV4cCI6MjEwMzE2NDU0NX0.mimHoEyyTYUNJFngqwex3Z4X-5xcv4SRN78P5CafrV8';
 
 let safeStorage;
 try {
